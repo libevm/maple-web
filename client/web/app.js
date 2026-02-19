af6eaf8449091bbea2d6eae732b2229b251df100
libevm <libevm@proton.me>
2026-02-19 16:13:55 +1100

    Face hotkey 3: shine (smug/showing off)

--- client/web/app.js
+++ client/web/app.js
@@ -8429,7 +8429,7 @@ function bindInput() {
     const FACE_HOTKEYS = {
       "Digit1": "hit",        // hit/pain
       "Digit2": "smile",      // happy
-      "Digit3": "dam",        // owned/rekt
+      "Digit3": "shine",      // smug/showing off
       "Digit4": "cry",        // cry
       "Digit5": "angry",      // angry
       "Digit6": "bewildered", // oooh/shocked
