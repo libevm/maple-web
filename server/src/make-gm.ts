04cf9ba13d657b13023850792e9308d4ab23d3d4
libevm <libevm@proton.me>
2026-02-21 17:24:52 +1100

    make-gm: add --db flag for custom DB path

--- server/src/make-gm.ts
+++ server/src/make-gm.ts
@@ -1,17 +1,28 @@
 #!/usr/bin/env bun
 /**
  * Toggle GM flag for a character.
- * Usage: bun run make-gm <username>
+ * Usage: bun run make-gm <username> [--db <path>]
  */
 import { initDatabase, isGm, setGm, characterExists } from "./db.ts";
 
-const name = process.argv[2];
+const args = process.argv.slice(2);
+let name = "";
+let dbPath = "./data/maple.db";
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === "--db" && args[i + 1]) {
+    dbPath = args[++i];
+  } else if (!name) {
+    name = args[i];
+  }
+}
+
 if (!name) {
-  console.error("Usage: bun run make-gm <username>");
+  console.error("Usage: bun run make-gm <username> [--db <path>]");
   process.exit(1);
 }
 
-const db = initDatabase("./data/maple.db");
+const db = initDatabase(dbPath);
 
 if (!characterExists(db, name)) {
   console.error(`Character '${name}' not found.`);
